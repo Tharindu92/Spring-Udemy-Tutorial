619b8e5ae72c71552cddbfe01832f9d1c2977f08
tharindu92 <tharindu.12@cse.mrt.ac.lk>
2020-02-04 21:13:36 +0800

    Folder Change Client

---
+++ lab3/lab-3-client/src/main/java/com/udemy/lab3client/controller/LuckyWorldController.java
@@ -0,0 +1,15 @@
+package com.udemy.lab3client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWorldController {
+    @Value("${lucky-word}") String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
